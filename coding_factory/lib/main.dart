f0af7f725b3f5aa57499388368d898af243c04c7
leetaeksoo <leets1490@gmail.com>
2022-08-17 11:19:04 +0900

    🚀 Splash screen 추가

--- coding_factory/lib/main.dart
+++ coding_factory/lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:coding_factory/screens/SplashScreen.dart';
 import 'package:flutter/material.dart';
 
 import 'package:coding_factory/screens/HelloWorldScreen.dart';
 
 void main(List<String> args) {
   runApp(MaterialApp(
-    home: HelloWorldScreen(),
+    // debugShowCheckedModeBanner: false, // debug 모드
+    // home: HelloWorldScreen(),
+    home: SplashScreen(),
   ));
 }
