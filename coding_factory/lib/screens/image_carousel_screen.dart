252d41c88e837073889c4af8aedf44c42ea5b24b
leetaeksoo <leets1490@gmail.com>
2022-08-23 10:51:05 +0900

    🚀 Image widget fit 적용

--- coding_factory/lib/screens/image_carousel_screen.dart
+++ coding_factory/lib/screens/image_carousel_screen.dart
@@ -13,10 +13,10 @@ class _ImageCarouselScreenState extends State<ImageCarouselScreen> {
     return Scaffold(
       body: PageView(
         children: [
-          Image.asset('asset/image_carousel/image_1.jpeg'),
-          Image.asset('asset/image_carousel/image_2.jpeg'),
-          Image.asset('asset/image_carousel/image_3.jpeg'),
-          Image.asset('asset/image_carousel/image_4.jpeg'),
+          Image.asset('asset/image_carousel/image_1.jpeg', fit: BoxFit.cover),
+          Image.asset('asset/image_carousel/image_2.jpeg', fit: BoxFit.cover),
+          Image.asset('asset/image_carousel/image_3.jpeg', fit: BoxFit.cover),
+          Image.asset('asset/image_carousel/image_4.jpeg', fit: BoxFit.cover),
         ],
       ),
     );
