9742eaf5b05a126f5747253fed6b8516b9f7e0ab
taeksoolee <leets1490@gmail.com>
2022-08-27 23:33:14 +0900

    🚀 rng - ThemeColor enum 추가

---
+++ random_number_generator/lib/constant/color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ThemeColors {
+  static Color primaryColor = const Color(0xFF2D2D33);
+  static Color redColor = const Color(0xFFEA4955);
+  static Color blueColor = const Color(0xFF549FBF);
+}
