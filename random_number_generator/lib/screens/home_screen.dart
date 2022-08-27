aa59f087e927b9a98b945b91fba2fe5a360f63c4
taeksoolee <leets1490@gmail.com>
2022-08-27 23:33:54 +0900

    🚀 rng - homescreen ThemeColor 등 레이아웃 스타일 적용

--- random_number_generator/lib/screens/home_screen.dart
+++ random_number_generator/lib/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:random_number_generator/constant/color.dart';
 import 'package:random_number_generator/widgets/countdown.dart';
 
 // 하남 배달특급 친구
@@ -17,35 +18,53 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: ThemeColors.primaryColor,
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('랜덤숫자 생성기'),
-                IconButton(onPressed: () {}, icon: Icon(Icons.settings))
-              ],
-            ),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 16.0,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text('123'),
-                  Text('456'),
-                  Text('789'),
+                  Text(
+                    '랜덤숫자 생성기',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.settings, color: ThemeColors.redColor))
                 ],
               ),
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text('Create!'),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('123'),
+                    Text('456'),
+                    Text('789'),
+                  ],
+                ),
               ),
-            ),
-          ],
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: ThemeColors.redColor,
+                  ),
+                  onPressed: () {},
+                  child: Text('Create!'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
