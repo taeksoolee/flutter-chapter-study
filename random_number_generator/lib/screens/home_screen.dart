4a8f6e72802660a46fda49a41f461abd17108db7
taeksoolee <leets1490@gmail.com>
2022-08-28 01:03:46 +0900

    🚀 rng - 코드 정리

--- random_number_generator/lib/screens/home_screen.dart
+++ random_number_generator/lib/screens/home_screen.dart
@@ -1,4 +1,3 @@
-import 'dart:async';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -19,7 +18,7 @@ class _HomeScreenState extends State<HomeScreen> {
   List<int> randomNumbers = [
     123,
     456,
-    213432,
+    789,
   ];
 
   @override
@@ -34,71 +33,103 @@ class _HomeScreenState extends State<HomeScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    '랜덤숫자 생성기',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 30.0,
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                  IconButton(
-                      onPressed: () {},
-                      icon: Icon(Icons.settings, color: ThemeColors.redColor))
-                ],
-              ),
-              Expanded(
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: randomNumbers
-                        .asMap()
-                        .entries
-                        .map((e) => Padding(
-                              padding:
-                                  EdgeInsets.only(top: e.key == 0 ? 0.0 : 16.0),
-                              child: Row(
-                                children: e.value
-                                    .toString()
-                                    .split('')
-                                    .map((e) => Image.asset(
-                                          'assets/img/$e.png',
-                                          width: 45.0,
-                                          height: 50.0,
-                                        ))
-                                    .toList(),
-                              ),
-                            ))
-                        .toList()),
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: ThemeColors.redColor,
-                  ),
-                  onPressed: () {
-                    final rand = Random();
-
-                    final Set<int> newNumbers = {};
-
-                    while (newNumbers.length != 3) {
-                      final number = rand.nextInt(1000);
-                      newNumbers.add(number);
-                    }
-
-                    setState(() {
-                      randomNumbers = newNumbers.toList();
-                    });
-                  },
-                  child: Text('Create!'),
-                ),
-              ),
+              _Header(),
+              _Body(randomNumbers: randomNumbers),
+              _Footer(onPressed: onRandomNumberGenerate)
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  onRandomNumberGenerate() {
+    final rand = Random();
+
+    final Set<int> newNumbers = {};
+
+    while (newNumbers.length != 3) {
+      final number = rand.nextInt(1000);
+      newNumbers.add(number);
+    }
+
+    setState(() {
+      randomNumbers = newNumbers.toList();
+    });
+  }
+}
+
+class _Header extends StatelessWidget {
+  const _Header({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          'Random Nums Generator',
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 20.0,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.settings, color: ThemeColors.redColor))
+      ],
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  final List<int> randomNumbers;
+
+  const _Body({required this.randomNumbers, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: randomNumbers
+              .asMap()
+              .entries
+              .map((e) => Padding(
+                    padding: EdgeInsets.only(top: e.key == 0 ? 0.0 : 16.0),
+                    child: Row(
+                      children: e.value
+                          .toString()
+                          .split('')
+                          .map((e) => Image.asset(
+                                'assets/img/$e.png',
+                                width: 45.0,
+                                height: 50.0,
+                              ))
+                          .toList(),
+                    ),
+                  ))
+              .toList()),
+    );
+  }
+}
+
+class _Footer extends StatelessWidget {
+  final void Function() onPressed;
+
+  const _Footer({required this.onPressed, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: ThemeColors.redColor,
+        ),
+        onPressed: onPressed,
+        child: const Text('Create!'),
       ),
     );
   }
