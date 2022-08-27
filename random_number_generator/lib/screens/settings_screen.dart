15a5b5938912f7f7938e3b6aff5ad16231a6c253
taeksoolee <leets1490@gmail.com>
2022-08-28 01:34:34 +0900

    🚀 rng - settings screen 레이아웃, Silder 위젯 추가

--- random_number_generator/lib/screens/settings_screen.dart
+++ random_number_generator/lib/screens/settings_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:random_number_generator/constant/color.dart';
 
 class SettingScreen extends StatefulWidget {
   const SettingScreen({Key? key}) : super(key: key);
@@ -10,11 +11,52 @@ class SettingScreen extends StatefulWidget {
 }
 
 class _SettingScreenState extends State<SettingScreen> {
+  double maxNumber = 10000.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('settings screen'),
+      backgroundColor: ThemeColors.primaryColor,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: Row(
+                  children: maxNumber
+                      .toInt()
+                      .toString()
+                      .split('')
+                      .map((e) => Image.asset(
+                            'assets/img/$e.png',
+                            width: 45.0,
+                            height: 50.0,
+                          ))
+                      .toList(),
+                ),
+              ),
+              Slider(
+                value: maxNumber.toDouble(),
+                min: 10000,
+                max: 1000000,
+                onChanged: (double val) {
+                  setState(() {
+                    maxNumber = val;
+                  });
+                },
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  primary: ThemeColors.redColor,
+                ),
+                child: const Text("Save!"),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
