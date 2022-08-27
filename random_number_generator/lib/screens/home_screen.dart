46d3ac99a0f538438483d111b2efcd45608dc521
taeksoolee <leets1490@gmail.com>
2022-08-27 19:06:11 +0900

    🚀 rng - 기본 레이아웃

--- random_number_generator/lib/screens/home_screen.dart
+++ random_number_generator/lib/screens/home_screen.dart
@@ -17,7 +17,37 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Countdown(),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('랜덤숫자 생성기'),
+                IconButton(onPressed: () {}, icon: Icon(Icons.settings))
+              ],
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('123'),
+                  Text('456'),
+                  Text('789'),
+                ],
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text('Create!'),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
