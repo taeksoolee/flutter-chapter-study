a78334efea0ac309fdafe3101ea229a9f9636d70
taeksoolee <leets1490@gmail.com>
2022-08-28 17:18:37 +0900

    🚀 rng - splash 화면중 fetch 코드 추가

--- random_number_generator/lib/main.dart
+++ random_number_generator/lib/main.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:random_number_generator/screens/home_screen.dart';
 
-void main() {
+Future<void> main() async {
+  await fetchApi();
+
   runApp(MaterialApp(
     home: HomeScreen(),
   ));
+}
+
+Future<bool> fetchApi() async {
+  bool result = false;
+
+  await Future.delayed(Duration(seconds: 3), () {
+    result = true;
+  });
+
+  return result;
 }
